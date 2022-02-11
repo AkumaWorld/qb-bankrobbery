d4297eaf9313d66f29d86318a3365f4091345be8
Arcadia Roleplay <arcadiaroleplaygtav@gmail.com>
2022-02-11 01:39:47 +0000

    Removed old thermite
    Removed old thermite mini game files

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,13 +1,9 @@
 fx_version 'cerulean'
 game 'gta5'
-
 description 'QB-BankRobbery'
 version '1.0.0'
 
-ui_page 'html/index.html'
-
 shared_script 'config.lua'
-
 client_scripts {
     'client/fleeca.lua',
     'client/pacific.lua',
@@ -15,11 +11,6 @@ client_scripts {
     'client/doors.lua',
     'client/paleto.lua',
 }
-
 server_script 'server/main.lua'
-
-files {
-    'html/*',
-}
 
 lua54 'yes'
